689abcef321eadbc2aaeac2069ab59499a291c66
Ai Ling <hong-ailing@hotmail.com>
2022-09-10 16:05:21 +0800

    Add ionic and react router dom

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import {
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router-dom';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Confused</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+const Test = () => {
+  return <p>abdcefg</p>;
+};
+
+function App() {
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/" exact component={Home} />
+          <Route path="/test" exact component={Test} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+}
+
+export default App;
