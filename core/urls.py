e226bae6a72cb127672950374f2c5d38a422984c
Kar Wi <t.karwi@yahoo.com>
2022-09-14 11:55:44 +0800

    Add viewsets to core

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+
+from rest_framework.routers import DefaultRouter
+
+from core import views
+
+
+router = DefaultRouter()
+router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
+
+
+urlpatterns = [path("", include(router.urls))]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+
+
+from core import serializers
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = ["test1", "test2", "test3"]
+
+        return Response({"message": "Hello!", "a_viewset": a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({"http_method": "GET"})
+
+    def update(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({"http_method": "PUT"})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({"http_method": "PATCH"})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({"http_method": "DELETE"})
