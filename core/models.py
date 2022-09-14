58f42c9ae8a677160e14336aee34550cb6d5285a
Kar Wi <t.karwi@yahoo.com>
2022-09-14 10:52:03 +0800

    Untrack and ignore env files

--- core/models.py
+++ core/models.py
@@ -4,53 +4,55 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserManager(BaseUserManager):
-  """Manager for user"""
-  
-  def create_instructor(self, email: str, name: str, password: str):
-    """Create an instructor"""
-    if not email:
-      raise ValueError("User must have an email address")
+    """Manager for user"""
 
-    email = self.normalize_email(email)
-    user: UserProfile = self.model(email=email, name=name)
+    def create_instructor(self, email: str, name: str, password: str):
+        """Create an instructor"""
+        if not email:
+            raise ValueError("User must have an email address")
 
-    user.set_password(password)
-    user.save(using=self._db)
+        email = self.normalize_email(email)
+        user: UserProfile = self.model(email=email, name=name)
 
-    return user
+        user.set_password(password)
+        user.save(using=self._db)
 
-  def create_superuser(self, email: str, name: str, password: str):
-    """Create a superuser"""
-    user = self.create_instructor(email, name, password)
+        return user
 
-    user.is_superuser = True 
-    user.is_staff = True 
-    user.save(using=self._db)
+    def create_superuser(self, email: str, name: str, password: str):
+        """Create a superuser"""
+        user = self.create_instructor(email, name, password)
 
-    return user
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-  """Database model for user"""
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)
+    """Database model for user"""
 
-  objects = UserManager()
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-  USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS: List[str] = ['name']
+    objects = UserManager()
 
-  def get_full_name(self):
-    """Retrieve full name of user"""
-    return self.name
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS: List[str] = ["name"]
 
-  def get_short_name(self):
-    """Retrieve short name of user"""
-    return self.name
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
 
-  def __str__(self):
-    """Return string representation of our user"""
-    return self.name
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.name
