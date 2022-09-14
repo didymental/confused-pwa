1c7ce718b6e26767e06a26e777480e0309cca8e6
Kar Wi <t.karwi@yahoo.com>
2022-09-14 15:28:24 +0800

    Create base view set

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from core import models
 
 
 from rest_framework.response import Response
@@ -10,52 +9,22 @@ from rest_framework import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
+from core import models
 from core import serializers
+from .pagination import StandardResultsSetPagination
 
 
-class HelloViewSet(viewsets.ViewSet):
-    """Test API ViewSet"""
+class BaseViewSet(viewsets.ModelViewSet):
+    """Base viewset for a viewset that handles CRUDL requests"""
 
-    serializer_class = serializers.HelloSerializer
-
-    def list(self, request):
-        """Return a hello message"""
-
-        a_viewset = ["test1", "test2", "test3"]
-
-        return Response({"message": "Hello!", "a_viewset": a_viewset})
-
-    def create(self, request):
-        """Create a new hello message"""
-        serializer = self.serializer_class(data=request.data)
-
-        if serializer.is_valid():
-            name = serializer.validated_data.get("name")
-            message = f"Hello {name}"
-            return Response({"message": message})
-
-        else:
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
-
-    def retrieve(self, request, pk=None):
-        """Handle getting an object by its ID"""
-        return Response({"http_method": "GET"})
-
-    def update(self, request, pk=None):
-        """Handle getting an object by its ID"""
-        return Response({"http_method": "PUT"})
-
-    def partial_update(self, request, pk=None):
-        """Handle updating part of an object"""
-        return Response({"http_method": "PATCH"})
-
-    def destroy(self, request, pk=None):
-        """Handle removing an object"""
-        return Response({"http_method": "DELETE"})
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardResultsSetPagination
+    ordering_fields = "__all__"
+    ordering = ["-id"]
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
