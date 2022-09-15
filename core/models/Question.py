05fa44eb5768312ca604430d2ed626835ef69e32
Didymus <e0544195@u.nus.edu>
2022-09-15 08:30:09 +0800

    Add models to project

---
+++ core/models/Question.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Question(models.Model):
+    """
+    Defines the Question that the Student can send in a Session.
+    """
+    student_id = models.ForeignKey("Student", on_delete=models.CASCADE)
+    vote_count = models.IntegerField()
+
+    # no submitting empty questions
+    question_content = models.CharField(
+        max_length=255, null=False, blank=False
+    )
+
+    def __str__(self):
+        return self.question_content
