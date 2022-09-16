cfae76a0fd3dbc4fda23f81cf330c1b0443dbd5f
Didymus <e0544195@u.nus.edu>
2022-09-16 20:05:32 +0800

    Update QuestionView GET endpoint

--- core/views/QuestionView.py
+++ core/views/QuestionView.py
@@ -1,8 +1,29 @@
 from core.views.BaseViewSet import BaseViewSet
 from ..models.Question import Question
+from ..models.Session import Session
+from ..models.Student import Student
 from ..serializers.QuestionSerializer import QuestionSerializer
 
 
 class QuestionView(BaseViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        """Returns questions that are asked in the session created by the instructor."""
+        result = []
+        sessions = Session.objects.filter(instructor=self.request.user)
+        students = Student.objects.all()
+
+        # get all students
+        students_in_session = []
+        for student in students:
+            if student.session_id in sessions:
+                students_in_session.append(student)
+
+        # get all questions
+        for question in self.queryset:
+            if question.student_id in students_in_session:
+                result.append(question)
+
+        return result
