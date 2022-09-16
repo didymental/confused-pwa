d09e37b488b4b4c3497f933b0a73b25bdf26d794
Didymus <e0544195@u.nus.edu>
2022-09-17 00:13:32 +0800

    Add bulk update and create functionality

--- core/views/StudentView.py
+++ core/views/StudentView.py
@@ -1,13 +1,28 @@
-from core.views.BaseViewSet import BaseViewSet
+from rest_framework_bulk import BulkModelViewSet
 from ..models.Student import Student
-from ..models.Session import Session
-from ..views.SessionView import SessionView
 from ..serializers.StudentSerializer import StudentSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from ..pagination import StandardResultsSetPagination
 
 
-class StudentView(BaseViewSet):
+class BaseBulkViewSet(BulkModelViewSet):
+    """Represents the interface for the Bulk Viewset"""
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    pagination_class = StandardResultsSetPagination
+    ordering_fields = "__all__"
+    ordering = ["-id"]
+
+    def allow_bulk_destroy(self, qs, filtered):
+        return False
+
+
+class StudentView(BaseBulkViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    model = Student
 
     def get_queryset(self):
         """Returns students that has joined the session by the instructor."""
