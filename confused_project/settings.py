a06783f8cb4955f6a7e73014d9ed3044f1573909
Kar Wi <t.karwi@yahoo.com>
2022-09-18 02:13:45 +0800

    Configuresettings for development

--- confused_project/settings.py
+++ confused_project/settings.py
@@ -97,15 +97,15 @@ WSGI_APPLICATION = "confused_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 
-DATABASES = {}
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+# DATABASES = {}
+# DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
