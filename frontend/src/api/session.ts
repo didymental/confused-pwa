8efa5678c557a0dd0b19e6cf606cdc6bbebd48ef
Ai Ling <hong-ailing@hotmail.com>
2022-09-19 20:31:38 +0800

    Integrate auth

--- frontend/src/api/session.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosResponse } from "axios";
-import client from "./client";
-
-// export async function listInstructorSessions(data: SignUpRequest): Promise<AxiosResponse> {
-//   return client.post<SignUpRequest>("/sessions/", data);
-// }
