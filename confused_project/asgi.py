0868a983f55e56c43694b907f309db5a36ab818e
Kar Wi <t.karwi@yahoo.com>
2022-09-19 16:22:29 +0800

    Fix something

--- confused_project/asgi.py
+++ confused_project/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 
@@ -27,6 +28,7 @@ else:
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE", "confused_project.settings.development"
     )
+django.setup()
 
 
 # TODO: https
