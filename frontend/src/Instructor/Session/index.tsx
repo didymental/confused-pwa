66a2f7aacbb63ada1e47d1f9044dfee8beb6af5b
Didymus <e0544195@u.nus.edu>
2022-09-19 23:34:35 +0800

    Upgrade UI for Instructor In Session

--- frontend/src/Instructor/Session/index.tsx
+++ frontend/src/Instructor/Session/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "./index.scss";
 import {
   IonButton,
@@ -11,11 +10,15 @@ import {
   IonListHeader,
   IonItem,
   IonCol,
+  IonIcon,
+  IonBadge,
+  IonText,
+  CreateAnimation,
 } from "@ionic/react";
 import React, { useEffect, useState } from "react";
+import { powerSharp } from "ionicons/icons";
 
 import client from "../../api/client";
-import Navbar from "../../component/Navbar";
 import { useToast } from "../../hooks/util/useToast";
 
 import clear from "../../assets/clear-bg.svg";
@@ -29,9 +32,9 @@ const CONFUSED_1_STATE = "confused-1";
 const CONFUSED_2_STATE = "confused-2";
 
 /**
- * Represents the page that shows the Instructor when the Instructor is 
- * in view of the session. 
- * 
+ * Represents the page that shows the Instructor when the Instructor is
+ * in view of the session.
+ *
  * @param sessionId the id of the Session the Instructor is in view of.
  */
 const InstructorSessionPage: React.FC<{ sessionId: number }> = ({ sessionId }) => {
@@ -43,21 +46,26 @@ const InstructorSessionPage: React.FC<{ sessionId: number }> = ({ sessionId }) =
   // Gets all students and questions in the session
   const getStudentsAndQuestionsInSession = (sessionId: number) => {
     client
-      .get("/students/").then(res => {
-        let studentsInCurrSession: StudentData[] = res.data.results
-          .filter((x: { session_id: number; }) => x.session_id === sessionId);
+      .get("/students/")
+      .then((res) => {
+        let studentsInCurrSession: StudentData[] = res.data.results.filter(
+          (x: { session: number }) => x.session === sessionId,
+        );
         setStudents(studentsInCurrSession);
 
         let setOfStudents: Set<number> = new Set();
-        studentsInCurrSession.forEach(student => setOfStudents.add(student.id));
+        studentsInCurrSession.forEach((student) => setOfStudents.add(student.id));
 
         client
           .get("/questions/")
-          .then(res => {
+          .then((res) => {
             let questionsFetched: QuestionData[] = res.data.results;
-            setQuestions(questionsFetched.filter(question => setOfStudents.has(question.student_id)));
+
+            setQuestions(
+              questionsFetched.filter((question) => setOfStudents.has(question.student)),
+            );
           })
-          .catch(err => {
+          .catch((err) => {
             presentToast({
               header: "Error occurred!",
               message: err.response.data.detail,
@@ -65,29 +73,41 @@ const InstructorSessionPage: React.FC<{ sessionId: number }> = ({ sessionId }) =
             });
           });
       })
-      .catch(err => {
+      .catch((err) => {
         presentToast({
           header: "Error occurred!",
           message: err.response.data.detail,
           color: "danger",
         });
       });
-  }
+  };
 
   useEffect(() => {
-    setLevelOfConfusion(CLEAR_STATE) // default to set as clear
-    getStudentsAndQuestionsInSession(sessionId)
+    sessionId = 3;
+    setLevelOfConfusion(CLEAR_STATE); // default to set as clear
+    getStudentsAndQuestionsInSession(sessionId);
   }, []);
 
   return (
     <IonPage>
-      <Navbar title={"Session Title"} />
-      <IonContent fullscreen>
+      <IonContent fullscreen className={`ion-content-instructor-session__${levelOfConfusion}`}>
+        {/* <IonButton
+          onClick={() =>
+            levelOfConfusion === CLEAR_STATE
+              ? setLevelOfConfusion(CONFUSED_1_STATE)
+              : levelOfConfusion === CONFUSED_1_STATE
+              ? setLevelOfConfusion(CONFUSED_2_STATE)
+              : setLevelOfConfusion(CLEAR_STATE)
+          }
+        >
+          Test
+        </IonButton> */}
         <ConfusionDisplay
           levelOfConfusion={levelOfConfusion || "clear"}
           questions={questions || []}
           students={students || []}
           sessionId={sessionId}
+          setLevelOfConfusion={setLevelOfConfusion}
         />
       </IonContent>
     </IonPage>
@@ -99,55 +119,70 @@ interface ConfusionDisplayProps {
   questions: QuestionData[] | [];
   students: StudentData[] | [];
   sessionId: number;
+  setLevelOfConfusion: (levelOfConfusion: string) => void;
 }
 
 const ConfusionDisplay: React.FC<ConfusionDisplayProps> = (props) => {
-  let { levelOfConfusion, questions, students, sessionId } = props;
+  let { levelOfConfusion, questions, students, sessionId, setLevelOfConfusion } = props;
+  const [hasAnimated, setHasAnimated] = useState(false);
   const { presentToast } = useToast();
+
+  useEffect(() => {
+    setHasAnimated(true);
+  }, [props]);
+
+  useEffect(() => {
+    if (hasAnimated) {
+      setHasAnimated(false);
+    }
+  }, [hasAnimated]);
 
   const endSession = () => {
     client
       .get(`/sessions/${sessionId}/`)
       .then((res) => {
         let toSend = { ...res.data, is_open: false };
-        client.put(`/sessions/${sessionId}/`, toSend)
-          .catch(err => {
-            presentToast({
-              header: "Error occurred!",
-              message: err.response.data.detail,
-              color: "danger",
-            });
+        client.put(`/sessions/${sessionId}/`, toSend).catch((err) => {
+          presentToast({
+            header: "Error occurred!",
+            message: err.response.data.detail,
+            color: "danger",
           });
+        });
       })
-      .catch(err => {
+      .catch((err) => {
         presentToast({
           header: "Error occurred!",
           message: err.response.data.detail,
           color: "danger",
         });
       });
-  }
+  };
 
   return (
-    <IonCard className={`ion-card-instructor-session__${levelOfConfusion}`}>
+    <>
+      <IonCard />
       <IonGrid>
+        <CreateAnimation
+          duration={2000}
+          iterations={1}
+          fromTo={[{ property: "opacity", fromValue: "0.1", toValue: "1" }]}
+          play={hasAnimated}
+        >
+          <IonRow className="ion-row-instructor-session">
+            <img src={mapLevelToSvg[levelOfConfusion]} alt={levelOfConfusion} onClick={() => {}} />
+          </IonRow>
+        </CreateAnimation>
         <IonRow className="ion-row-instructor-session">
-          <img
-            src={mapLevelToSvg[levelOfConfusion]}
-            alt={levelOfConfusion}
-            onClick={() => { }}
-          />
-        </IonRow>
-        <IonRow className="ion-row-instructor-session">
-          <h5>
+          <IonText className="ion-text-instructor-session">
             {mapLevelToText[levelOfConfusion]}
-          </h5>
+          </IonText>
         </IonRow>
         <IonRow className="ion-row-instructor-session">
           <QuestionsDisplay questions={questions} />
         </IonRow>
         <IonRow className="ion-row-instructor-session">
-          <ReactionsDisplay students={students} levelOfConfusion={levelOfConfusion} />
+          <ReactionsDisplay students={students} setLevelOfConfusion={setLevelOfConfusion} />
         </IonRow>
         <IonRow className="ion-row-instructor-session">
           <IonButton
@@ -155,61 +190,67 @@ const ConfusionDisplay: React.FC<ConfusionDisplayProps> = (props) => {
             fill="clear"
             onClick={endSession}
           >
-            End Session
+            <IonIcon icon={powerSharp} size="small" />
           </IonButton>
         </IonRow>
       </IonGrid>
-    </IonCard >
+    </>
   );
-}
+};
 
 /**
- * Defines the structure of the QuestionData that 
+ * Defines the structure of the QuestionData that
  * is received from the backend.
  */
 interface QuestionData {
-  student_id: number,
-  question_content: string,
-  vote_count: number
+  student: number;
+  question_content: string;
+  vote_count: number;
 }
 
 /**
- * Defines the structure of the StudentData that 
+ * Defines the structure of the StudentData that
  * is received from the backend.
  */
 interface StudentData {
-  id: number,
-  display_name: string,
-  session_id: number,
-  reaction_type_id: number | null
+  id: number;
+  display_name: string;
+  session: number;
+  reaction_type_id: number | null;
 }
 
 const QuestionsDisplay: React.FC<{ questions: QuestionData[] | [] }> = ({ questions }) => {
-  return (
-    questions.length === 0
-      ? null
-      : <IonList className="ion-list-instructor-session__questions">
+  return questions.length === 0 ? null : (
+    <IonCard className="ion-card-instructor-session__questions">
+      <IonList className="ion-list-instructor-session__questions">
         <IonListHeader className="ion-list-header-instructor-session__questions">
           Questions from Students
         </IonListHeader>
-        <IonGrid>
-          {questions.map((question: QuestionData) =>
-            <IonItem className="ion-item-instructor-session__questions">
-              {question.question_content}
-            </IonItem>)}
-        </IonGrid>
+        {questions.map((question: QuestionData) => (
+          <IonItem
+            className="ion-item-instructor-session__questions"
+            key={question.question_content}
+          >
+            {question.question_content}
+          </IonItem>
+        ))}
       </IonList>
+    </IonCard>
   );
-}
+};
 
-const ReactionsDisplay: React.FC<{ students: StudentData[] | [], levelOfConfusion: string }> = ({ students, levelOfConfusion }) => {
-  const [countOfClear, setCountOfClear] = useState(0);
-  const [countOfConfused, setCountOfConfused] = useState(0);
+const ReactionsDisplay: React.FC<{
+  students: StudentData[] | [];
+  setLevelOfConfusion: (levelOfConfusion: string) => void;
+}> = ({ students, setLevelOfConfusion }) => {
+  const [countOfClear, setCountOfClear] = useState<number>(0);
+  const [countOfConfused, setCountOfConfused] = useState<number>(0);
+  const [ratio, setRatio] = useState<number>(0.5);
 
   useEffect(() => {
     let countClear = 0;
     let countConfused = 0;
-    students.forEach(student => {
+    students.forEach((student) => {
       if (student.reaction_type_id === 2) {
         countClear += 1;
       }
@@ -221,57 +262,81 @@ const ReactionsDisplay: React.FC<{ students: StudentData[] | [], levelOfConfusio
 
     setCountOfClear(countClear);
     setCountOfConfused(countConfused);
-  }, [students])
+
+    if (countOfClear === 0 && countOfConfused === 0) {
+      setRatio(0.5);
+    } else {
+      let ratioOfConfused = countConfused / (countClear + countConfused);
+      setRatio(ratioOfConfused);
+    }
+  }, [students]);
+
+  useEffect(() => {
+    if (ratio > 0.7) {
+      setLevelOfConfusion(CONFUSED_1_STATE);
+      return () => {
+        setLevelOfConfusion(CLEAR_STATE);
+      };
+    }
+
+    if (ratio > 0.9) {
+      setLevelOfConfusion(CONFUSED_2_STATE);
+      return () => {
+        setLevelOfConfusion(CLEAR_STATE);
+      };
+    }
+
+    setLevelOfConfusion(CLEAR_STATE);
+  }, [ratio]);
 
   const clearReactions = () => {
     let toSend = [];
     for (let i = 0; i < students.length; i++) {
-      let studentCopy = { ...students[i] }
+      let studentCopy = { ...students[i] };
       studentCopy.reaction_type_id = null;
       toSend.push(studentCopy);
     }
     client.put("/students/", toSend);
-  }
+  };
 
   return (
-    <IonGrid>
-      <IonRow>
+    <IonCard className="ion-card-instructor-session__reactions">
+      <IonGrid>
+        <IonRow>
+          <IonCol className="ion-col-instructor-session__reactions">
+            <IonRow>
+              <img src={confused_reaction} alt={"confused"} onClick={() => {}} />
+            </IonRow>
+            <IonRow>
+              <IonBadge className="ion-badge-instructor-session__reactions" color="light">
+                <IonText>{countOfConfused}</IonText>
+              </IonBadge>
+            </IonRow>
+          </IonCol>
+          <IonCol className="ion-col-instructor-session__reactions">
+            <IonRow>
+              <img src={clear_reaction} alt={"clear"} onClick={() => {}} />
+            </IonRow>
+            <IonRow>
+              <IonBadge className="ion-badge-instructor-session__reactions" color="light">
+                <IonText>{countOfClear}</IonText>
+              </IonBadge>
+            </IonRow>
+          </IonCol>
+        </IonRow>
         <IonCol className="ion-col-instructor-session__reactions">
-          <img
-            src={confused_reaction}
-            alt={"confused"}
-            onClick={() => { }}
-          />
-          <IonCard className="ion-card-instructor-session__reactions">
-            {countOfConfused}
-          </IonCard>
+          <IonButton
+            className="ion-btn-instructor-session__reactions"
+            onClick={clearReactions}
+            fill="clear"
+          >
+            <IonText>Reset</IonText>
+          </IonButton>
         </IonCol>
-        <IonCol className="ion-col-instructor-session__reactions">
-          <img
-            src={clear_reaction}
-            alt={"clear"}
-            onClick={() => { }}
-          />
-          <IonCard className="ion-card-instructor-session__reactions">
-            {countOfClear}
-          </IonCard>
-        </IonCol>
-      </IonRow>
-      {
-        levelOfConfusion === CONFUSED_2_STATE
-          ? <IonRow className="ion-row-instructor-session__reactions">
-            <IonButton
-              className="ion-btn-instructor-session__reactions"
-              onClick={clearReactions}
-            >
-              Clear Reactions
-            </IonButton>
-          </IonRow>
-          : null
-      }
-    </IonGrid>
+      </IonGrid>
+    </IonCard>
   );
-}
+};
 
 /**
  * Finite state machine mapping the levelOfConfusion to svg component
@@ -279,16 +344,16 @@ const ReactionsDisplay: React.FC<{ students: StudentData[] | [], levelOfConfusio
 const mapLevelToSvg: { [key: string]: any } = {
   [CLEAR_STATE]: clear,
   [CONFUSED_1_STATE]: confused_1,
-  [CONFUSED_2_STATE]: confused_2
+  [CONFUSED_2_STATE]: confused_2,
 };
 
 /**
  * Finite state machine mapping the levelOfConfusion to text displayed
  */
 const mapLevelToText: { [key: string]: string } = {
-  "clear": "I am clear!",
+  clear: "I am clear!",
   "confused-1": "I am confused...",
-  "confused-2": "I am really confused..."
-}
+  "confused-2": "I am really confused...",
+};
 
 export default InstructorSessionPage;
