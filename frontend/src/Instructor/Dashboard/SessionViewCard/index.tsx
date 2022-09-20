8a19a98d2c9a1955186fa6570984c116891608ad
Ai Ling <hong-ailing@hotmail.com>
2022-09-20 20:14:42 +0800

    Format date

--- frontend/src/Instructor/Dashboard/SessionViewCard/index.tsx
+++ frontend/src/Instructor/Dashboard/SessionViewCard/index.tsx
@@ -15,14 +15,13 @@ import { createOutline, trashOutline, ellipsisVertical } from "ionicons/icons";
 import { useHistory } from "react-router";
 import { useSessions } from "../../../hooks/session/useSession";
 import { SessionEntity } from "../../../types/session";
+import { getFormattedDate } from "../../../utils/date";
 import "./index.scss";
 
 const SessionViewCard: React.FC<SessionEntity> = (session) => {
-  const sessionId = session.id;
-  const name = session.name;
-  const isOpen = session.is_open;
+  const { id: sessionId, name, is_open: isOpen, created_date_time: dateTime } = session;
   // TODO: convert to correct date format
-  const createdAt = session.created_date_time ?? null;
+  const createdDate = dateTime ? getFormattedDate(dateTime) : null;
 
   const history = useHistory();
   const { deleteSession } = useSessions();
@@ -63,8 +62,8 @@ const SessionViewCard: React.FC<SessionEntity> = (session) => {
                 <IonRow>
                   <IonCardTitle>{name}</IonCardTitle>
                 </IonRow>
-                <IonRow>
-                  <IonCardSubtitle>{`Created at: ${createdAt}`}</IonCardSubtitle>
+                <IonRow className="card__subtitle">
+                  <IonCardSubtitle>{`Created on ${createdDate}`}</IonCardSubtitle>
                 </IonRow>
               </IonCol>
               <IonCol className="dashboard__menu" size="2">
